3decded0c3a1dd155ab3c263f8d2298d923a8108
Tin Phan <tnphan4@gmail.com>
2020-02-13 14:12:42 -0800

    Change profilr layout + chat popup + Add tab that shows in closet

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -2003,4 +2003,20 @@ if (typeof jQuery === "undefined") { throw new Error("Bootstrap requires jQuery"
     })
   })
 
+//For Profile Menu
+  window.fn = {};
+
+window.fn.open = function() {
+  var menu = document.getElementById('menu');
+  menu.open();
+};
+
+window.fn.load = function(page) {
+  var content = document.getElementById('content');
+  var menu = document.getElementById('menu');
+  content.load(page)
+    .then(menu.close.bind(menu));
+};
+
+
 }(jQuery);
