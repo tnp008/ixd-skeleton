355da63648da3849fe807d12804f4ef78be83033
TIN PHAN <tnphan4@gmail.com>
2020-02-13 18:24:53 -0800

    Add files via upload

---
+++ routes/add.js
@@ -0,0 +1,11 @@
+var data = require("../data.json");
+
+exports.addItem = function(request, response) {    
+	// Your code goes here
+	
+	data.add.push({"brand": request.query.brand,
+		"description": request.query.description, 
+		"imageURL": "https://i.pinimg.com/originals/47/85/cb/4785cb6aa6e544d57908dd49d38d796f.jpg" });
+	console.log(data)
+	response.render('index', data);
+ }
