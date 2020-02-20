7db8f83e0df595afcffd4ac2f6dc14d7a4bdfda8
Kristine Marie Baluyot <kmbaluyo@ucsd.edu>
2020-02-19 19:15:22 -0800

    notification page done

--- routes/add.js
+++ routes/add.js
@@ -6,6 +6,7 @@ exports.addItem = function(request, response) { 
 	data.add.push({"brand": request.query.brand,
 		"description": request.query.description, 
 		"imageURL": "https://i.pinimg.com/originals/47/85/cb/4785cb6aa6e544d57908dd49d38d796f.jpg" });
+
 	console.log(data)
 	response.render('index', data);
  }
