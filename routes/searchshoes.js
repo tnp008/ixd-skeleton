11564325e3ab4e6f5a95bd5c9f0560fdb2dfeaa0
Sophia <sodoming@ucsd.edu>
2020-03-02 20:25:29 -0800

    search function

--- routes/searchshoes.js
+++ routes/searchshoes.js
@@ -1,4 +1,3 @@
-
 /*
  * GET home page.
  */
@@ -6,19 +5,44 @@
 var data = require('../data.json');
 
 exports.view = function(request, response){
-	var arr = data['shoes'];
-	var answer = [];
 	var user_key = request.query.brand;
-	console.log(data['shoes']);
 
-	for(var i = 0; i < arr.length; i++){
-		if (arr[i]["brand"] == user_key){
-			answer.push(arr[i]);
+	var arr_shoes = data['shoes'];
+	var answer_shoes = [];
+	
+	var arr_shirts = data['shirts'];
+	var answer_shirts = [];
+
+	var arr_bottoms = data['bottoms'];
+	var answer_bottoms = [];
+
+	var arr_outerwear = data['outerwear'];
+	var answer_outerwear = [];
+	
+	for(var i = 0; i < arr_shoes.length; i++){
+		if (arr_shoes[i]["brand"] == user_key){
+			answer_shoes.push(arr_shoes[i]);
 		}
 	}
-
+	for(var i = 0; i < arr_shirts.length; i++){
+		if (arr_shirts[i]["brand"] == user_key){
+			answer_shirts.push(arr_shirts[i]);
+		}
+	}
+	for(var i = 0; i < arr_bottoms.length; i++){
+		if (arr_bottoms[i]["brand"] == user_key){
+			answer_bottoms.push(arr_bottoms[i]);
+		}
+	}
+	for(var i = 0; i < arr_outerwear.length; i++){
+		if (arr_outerwear[i]["brand"] == user_key){
+			answer_outerwear.push(arr_outerwear[i]);
+		}
+	}
 	response.render('index', {
-		"shoes": answer,
-		"clothes" : []
+		"shoes": answer_shoes,
+		"shirts": answer_shirts,
+		"bottoms": answer_bottoms,
+		"outerwear":answer_outerwear		
 	});
 };
